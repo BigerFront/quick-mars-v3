0d67b63c22ace5e2fb525903c1469981460ecb0e
developer <team-developer@gmail.com>
2021-04-28 16:19:45 +0800

    upd: docs

--- src/ui/widgets/QuickDialIcon.vue
+++ src/ui/widgets/QuickDialIcon.vue
@@ -16,8 +16,8 @@
         <v-icon v-else>mdi-menu</v-icon>
       </v-btn>
     </template>
-    <v-btn fab dark small color="green">
-      <v-icon>{{ icons.fullscreen }}</v-icon>
+    <v-btn fab dark small color="green" @click="toggleFullscreenHandle">
+      <v-icon>{{ fullIcon }}</v-icon>
     </v-btn>
     <v-btn fab dark small color="indigo" @click="gobackHome">
       <v-icon>mdi-home</v-icon>
@@ -28,6 +28,34 @@
 <script>
 import { mapGetters } from 'vuex';
 
+const toggleFullscreen = () => {
+  const doc = window.document;
+  const docEl = doc.documentElement;
+
+  const requestFullScreen =
+    docEl.requestFullscreen ||
+    docEl.mozRequestFullScreen ||
+    docEl.webkitRequestFullScreen ||
+    docEl.msRequestFullscreen;
+  const cancelFullScreen =
+    doc.exitFullscreen ||
+    doc.mozCancelFullScreen ||
+    doc.webkitExitFullscreen ||
+    doc.msExitFullscreen;
+
+  if (
+    !doc.fullscreenElement &&
+    !doc.mozFullScreenElement &&
+    !doc.webkitFullscreenElement &&
+    !doc.msFullscreenElement
+  ) {
+    requestFullScreen.call(docEl);
+    return true;
+  }
+
+  cancelFullScreen.call(doc);
+  return false;
+};
 export default {
   name: 'QuickDialIcon',
   components: {},
@@ -46,11 +74,21 @@ export default {
     };
   },
   computed: {
-    ...mapGetters('ui', ['icons']),
+    ...mapGetters('ui', ['fullscreenShow', 'icons']),
+    fullIcon() {
+      return this.fullscreenShow
+        ? this.icons.fullscreenExit
+        : this.icons.fullscreen;
+    },
   },
   methods: {
     gobackHome() {
       this.$router.replace({ path: '/' });
+    },
+    toggleFullscreenHandle() {
+      this.$store.dispatch('ui/toggleFullscreen', {
+        fullState: toggleFullscreen(),
+      });
     },
   },
 };
